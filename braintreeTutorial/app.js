00ba499ea23c9923977b26fe7819c9b007a15945
ALJorge <jorgealvarezlu@gmail.com>
2018-03-05 18:23:43 -0600

    Falta mostrar articulos

--- braintreeTutorial/app.js
+++ braintreeTutorial/app.js
@@ -24,7 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-
+// The checkout route
+var checkout = require('./routes/checkout');
+app.use('/checkout', checkout);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
